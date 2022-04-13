3cd7c41ff802857eda5868b44b50093b0d946507
Matt Lim <mlimcollege@gmail.com>
2022-04-12 23:44:31 -0700

    Implement vote program

--- programs/vote-program/src/lib.rs
+++ programs/vote-program/src/lib.rs
@@ -2,14 +2,99 @@ use anchor_lang::prelude::*;
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
+const VOTE_MARKER: &str = "vote_marker";
+const VOTE_TOPIC: &str = "vote_topic";
+pub const MAX_NAME_LENGTH: usize = 32;
+
 #[program]
 pub mod vote_program {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn create_vote_topic<'info>(ctx: Context<CreateVoteTopic>, name: String) -> Result<()> {
+        let creator = &ctx.accounts.creator;
+        let vote_topic = &mut ctx.accounts.vote_topic;
+
+        if name.len() > MAX_NAME_LENGTH {
+            return Err(ErrorCode::NameTooLong.into());
+        }
+
+        vote_topic.downvotes = 0;
+        vote_topic.upvotes = 0;
+
+        vote_topic.creator = creator.key();
+        vote_topic.name = name;
+
+        Ok(())
+    }
+
+    pub fn vote<'info>(ctx: Context<Vote>, _name: String, upvote: bool) -> Result<()> {
+        let vote_topic = &mut ctx.accounts.vote_topic;
+
+        if upvote {
+            vote_topic.upvotes += 1;
+        } else {
+            vote_topic.downvotes += 1;
+        }
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(name: String)]
+pub struct CreateVoteTopic<'info> {
+    #[account(mut)]
+    creator: Signer<'info>,
+
+    #[account(init, payer = creator, space = VOTE_TOPIC_SIZE, seeds=[VOTE_TOPIC.as_bytes(), creator.key().as_ref(), name.as_bytes()], bump)]
+    vote_topic: Account<'info, VoteTopic>,
+
+    system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(name: String)]
+pub struct Vote<'info> {
+    #[account(mut)]
+    voter: Signer<'info>,
+
+    /// CHECK: no validation necessary
+    creator: UncheckedAccount<'info>,
+
+    #[account(mut, seeds=[VOTE_TOPIC.as_bytes(), creator.key().as_ref(), name.as_bytes()], bump, has_one=creator)]
+    vote_topic: Account<'info, VoteTopic>,
+
+    #[account(init, payer = voter, space = VOTE_MARKER_SIZE, seeds=[VOTE_MARKER.as_bytes(), vote_topic.key().as_ref(), voter.key().as_ref()], bump)]
+    vote_marker: Account<'info, VoteTopic>,
+
+    system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct VoteTopic {
+    creator: Pubkey,
+    downvotes: u32,
+    name: String,
+    upvotes: u32,
+}
+
+#[account]
+pub struct VoteMarker {
+    bump: u8,
+}
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("Name too long")]
+    NameTooLong,
+}
+
+pub const VOTE_MARKER_SIZE: usize = 8 + // discriminator
+128; // padding
+
+pub const VOTE_TOPIC_SIZE: usize = 8 + // discriminator
+32 + // creator
+4 + // downvotes
+32 + // name
+4 + // upvotes
+128; // padding
--- tests/vote-program.ts
+++ tests/vote-program.ts
@@ -1,16 +1,125 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
+import {
+  Connection,
+  Keypair,
+  LAMPORTS_PER_SOL,
+  PublicKey,
+  SystemProgram,
+} from "@solana/web3.js";
+import chai, { assert, expect } from "chai";
 import { VoteProgram } from "../target/types/vote_program";
+import chaiAsPromised from "chai-as-promised";
+chai.use(chaiAsPromised);
+
+// Configure the client to use the local cluster.
+anchor.setProvider(anchor.AnchorProvider.env());
+
+const program = anchor.workspace.VoteProgram as Program<VoteProgram>;
+const CREATOR = Keypair.generate();
+const VOTER = Keypair.generate();
+
+async function findVoteMarkerAddress(voteTopic: PublicKey, voter: PublicKey) {
+  return PublicKey.findProgramAddress(
+    [Buffer.from("vote_marker"), voteTopic.toBuffer(), voter.toBuffer()],
+    program.programId
+  );
+}
+
+async function findVoteTopicAddress(creator: PublicKey, name: string) {
+  return PublicKey.findProgramAddress(
+    [Buffer.from("vote_topic"), creator.toBuffer(), Buffer.from(name)],
+    program.programId
+  );
+}
+
+async function requestAirdrops(
+  wallets: Array<Keypair>,
+  connection: Connection
+) {
+  await Promise.all(
+    wallets.map(async (wallet) => {
+      const airdrop = await connection.requestAirdrop(
+        wallet.publicKey,
+        50 * LAMPORTS_PER_SOL
+      );
+      await connection.confirmTransaction(airdrop, "confirmed");
+    })
+  );
+}
+
+async function createVoteTopic(creator: Keypair, name: string) {
+  const [voteTopicAddress] = await findVoteTopicAddress(
+    creator.publicKey,
+    name
+  );
+  await program.methods
+    .createVoteTopic(name)
+    .accounts({
+      creator: creator.publicKey,
+      systemProgram: SystemProgram.programId,
+      voteTopic: voteTopicAddress,
+    })
+    .signers([creator])
+    .rpc();
+  const voteTopicAccount = await program.account.voteTopic.fetch(
+    voteTopicAddress
+  );
+
+  expect(voteTopicAccount.creator.toString()).eq(creator.publicKey.toString());
+  expect(voteTopicAccount.downvotes).eq(0);
+  expect(voteTopicAccount.name).eq(name);
+  expect(voteTopicAccount.upvotes).eq(0);
+
+  return voteTopicAddress;
+}
 
 describe("vote-program", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  before(async () => {
+    await requestAirdrops([CREATOR, VOTER], program.provider.connection);
+  });
 
-  const program = anchor.workspace.VoteProgram as Program<VoteProgram>;
+  it("Create vote topic", async () => {
+    await createVoteTopic(CREATOR, "test");
+  });
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  it("Vote on topic", async () => {
+    const name = "test2";
+    const voteTopicAddress = await createVoteTopic(CREATOR, name);
+
+    const [voteMarkerAddress] = await findVoteMarkerAddress(
+      voteTopicAddress,
+      VOTER.publicKey
+    );
+
+    await program.methods
+      .vote(name, true)
+      .accounts({
+        voter: VOTER.publicKey,
+        creator: CREATOR.publicKey,
+        voteTopic: voteTopicAddress,
+        voteMarker: voteMarkerAddress,
+        systemProgram: SystemProgram.programId,
+      })
+      .signers([VOTER])
+      .rpc();
+
+    const voteTopicAccount = await program.account.voteTopic.fetch(
+      voteTopicAddress
+    );
+    expect(voteTopicAccount.upvotes).eq(1);
+
+    const promise = program.methods
+      .vote(name, true)
+      .accounts({
+        voter: VOTER.publicKey,
+        creator: CREATOR.publicKey,
+        voteTopic: voteTopicAddress,
+        voteMarker: voteMarkerAddress,
+        systemProgram: SystemProgram.programId,
+      })
+      .signers([VOTER])
+      .rpc();
+    await assert.isRejected(promise);
   });
 });
